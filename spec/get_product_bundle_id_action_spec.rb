f1621683a43c4d10f024b0efc235e0f8a4254a07
Lukas Würzburger <lukas.wuerzburger@truffls.com>
2019-11-26 15:54:32 +0100

    add tests for the build configuration parameter
    - add build configurations 'Debug Stage' and 'Release Stage' in the demo app
    - add target 'CoinTossingManager' in the demo app
    - add scheme 'MoneyShakerManager'

--- spec/get_product_bundle_id_action_spec.rb
+++ spec/get_product_bundle_id_action_spec.rb
@@ -60,6 +60,18 @@ describe Fastlane do
           )
         end
 
+        it 'throws an error when the given build configuration does not exist' do
+          project_path = "#{@tmp_xcodeproj_dirpath}/CoinTossing.xcodeproj"
+          fastfile = "lane :test do
+            get_product_bundle_id(project_filepath: '#{project_path}', scheme: 'MoneyShaker', build_configuration: 'No Build Configuration')
+          end"
+          expect { Fastlane::FastFile.new.parse(fastfile).runner.execute(:test) }.to(
+            raise_error(FastlaneCore::Interface::FastlaneError) do |error|
+              expect(error.message).to match("Build configuration 'No Build Configuration' does not exist in target 'MoneyShaker'")
+            end
+          )
+        end
+
         it 'provides the PRODUCT_BUNDLE_IDENTIFIER for the first target in the scheme' do
           project_path = "#{@tmp_xcodeproj_dirpath}/CoinTossing.xcodeproj"
           fastfile = "lane :test do
@@ -76,6 +88,24 @@ describe Fastlane do
           end"
           result = Fastlane::FastFile.new.parse(fastfile).runner.execute(:test)
           expect(result).to eq("Test.CoinTossing")
+        end
+
+        it 'provides the PRODUCT_BUNDLE_IDENTIFIER for the first target in the scheme with the debug build configuration' do
+          project_path = "#{@tmp_xcodeproj_dirpath}/CoinTossing.xcodeproj"
+          fastfile = "lane :test do
+            get_product_bundle_id(project_filepath: '#{project_path}', scheme: 'MoneyShakerManager', target: 'CoinTossingManager', build_configuration: 'Debug')
+          end"
+          result = Fastlane::FastFile.new.parse(fastfile).runner.execute(:test)
+          expect(result).to eq("Test.CoinTossingManager")
+        end
+
+        it 'provides the PRODUCT_BUNDLE_IDENTIFIER for the first target in the scheme with the stage build configuration' do
+          project_path = "#{@tmp_xcodeproj_dirpath}/CoinTossing.xcodeproj"
+          fastfile = "lane :test do
+            get_product_bundle_id(project_filepath: '#{project_path}', scheme: 'MoneyShakerManager', target: 'CoinTossingManager', build_configuration: 'Debug Stage')
+          end"
+          result = Fastlane::FastFile.new.parse(fastfile).runner.execute(:test)
+          expect(result).to eq("Test.CoinTossingManager.Stage")
         end
       end
     end
